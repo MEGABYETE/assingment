ef0d1e73ea436ad7c9c0bb03e33402b83813c8f3
MEGABYETE <101935068+MEGABYETE@users.noreply.github.com>
2022-10-16 17:17:06 +0100

    Add files via upload

---
+++ s.py
@@ -0,0 +1,7 @@
+#Area of a circle
+#(Radius*22/7)
+Radius=int(input("Enter your Radius"))
+Pi=int(input("Enter your Pi"))
+print("The area of the circle is", area_of_circle)
+#comparasion operators
+#Logical Operators
